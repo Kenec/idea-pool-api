591f29ec4a8fc11b39ce98a832e6db1325dfce96
Kenec <nnamani.kenechukwu@gmail.com>
2019-03-20 14:56:57 +0100

    build the logout endpoint

--- server/controllers/accessTokens.js
+++ server/controllers/accessTokens.js
@@ -18,7 +18,9 @@ export default class AccessTokens {
     const refreshToken = req.body.refresh_token;
 
     client.get(refreshToken, (error, response) => {
-      if (JSON.parse(response).refresh_token != null) {
+      if (response != null) {
+        if (!JSON.parse(response).refresh_token) return res.status(404).json({ error: 'No refresh token found' });
+
         jwt.verify(JSON.parse(response).refresh_token, process.env.JWT_REFRESH_TOKEN, (err, authToken) => {
           if (err) {
             return res.status(401).json({ message: 'Refresh Token is Invalid' });
@@ -36,7 +38,7 @@ export default class AccessTokens {
           }
         });
       } else {
-        return res.status(404).json({ error: 'Invalid Request '});
+        return res.status(404).json({ error: 'No token found!' });
       }
     });
   }
@@ -51,11 +53,11 @@ export default class AccessTokens {
     const { email, password } = req.body;
 
     User.findOne({ email }, (error, user) => {
-      if (error) return res.status(500).send({ error: 'There seems to be some issue. Try agin!' });
-      if (user === null) return res.status(401).send({ error: 'User does not exist' });
+      if (error) return res.status(500).json({ error: 'There seems to be some issue. Try agin!' });
+      if (user === null) return res.status(401).json({ error: 'User does not exist' });
       
       const validPassword = bcrypt.compareSync(password, user.password);
-      if (!validPassword) return res.status(401).send({ error: 'User does not exits' });
+      if (!validPassword) return res.status(401).json({ error: 'User does not exits' });
 
       const token = jwt.sign({ id: user._id, name: user.name, email: user.email }, process.env.JWT_ACCESS_TOKEN, { expiresIn: 600 });
       const refresh_token = jwt.sign({ id: user._id, name: user.name, email: user.email }, process.env.JWT_REFRESH_TOKEN, { expiresIn: 86400 });
@@ -64,6 +66,38 @@ export default class AccessTokens {
       client.set(refresh_token, JSON.stringify(response));
 
       return res.status(201).json(response);
+    });
+  }
+
+  /**
+   * logout - Removes authentication token for a user
+   * @param {object} req 
+   * @param {object} res
+   * @returns {object}
+   */
+  static logout(req, res) {
+    client.get(req.body.refresh_token, (error, response) => {
+      if (response != null) {
+        if (!JSON.parse(response).refresh_token) return res.status(404).json({ error: 'No refresh token found' });
+
+        jwt.verify(JSON.parse(response).refresh_token, process.env.JWT_REFRESH_TOKEN, (err, authToken) => {
+          if (err) {
+            return res.status(401).json({ message: 'Refresh Token is Invalid' });
+          } else {
+            client.del(req.body.refresh_token, (error, response) => {
+              if (response === 1) {
+                delete req.headers['x-access-token'];
+                console.log(req.headers);
+                return res.status(204).json({})
+              } else {
+                return res.status(200).json({ message: 'User not logged out '});
+              }
+            });
+          }
+        });
+      } else {
+        return res.status(404).json({ error: 'No token found! '});
+      }
     });
   }
 
--- server/routes/accessTokensRoutes.js
+++ server/routes/accessTokensRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import accessTokenController from '../controllers/accessTokens';
 import accessTokenMiddleware from '../middlewares/accessTokens';
 import userMiddleware from '../middlewares/user';
+import jwtAuth from '../middlewares/jwtAuth';
 
 const accessTokensRoutes = express.Router();
 
@@ -9,6 +10,11 @@ accessTokensRoutes.post('/',
   userMiddleware.validateEmail,
   userMiddleware.validatePassword,
   accessTokenController.login);
+
+accessTokensRoutes.delete('/',
+  accessTokenMiddleware.validateRefreshToken,
+  jwtAuth,
+  accessTokenController.logout)
 
 accessTokensRoutes.post('/refresh',
   accessTokenMiddleware.validateRefreshToken,
